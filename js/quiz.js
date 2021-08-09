65c6f8e494da9585f30a56c3fe1bb99d9d0d5459
Avaneesh Kumar <avaneeshk098@gmail.com>
2021-08-09 23:12:19 +0530

    added fallback api

--- js/quiz.js
+++ js/quiz.js
@@ -1,45 +1,49 @@
 window.onload = () => {
-    if(localStorage.getItem('filled') === true) {
-        document.querySelector('.Discord').style.display = 'flex';
-        document.querySelector('.correct').style.display = 'none';
-        document.querySelector('.qa').style.display = "none";
-        document.querySelector('.incorrect').style.display = "none";
+    if (localStorage.getItem('filled') === 'true') {
+        document.querySelector('.Discord').style.display = 'flex'
+        document.querySelector('.correct').style.display = 'none'
+        document.querySelector('.qa').style.display = 'none'
+        document.querySelector('.incorrect').style.display = 'none'
     }
     let questions = {
-        1: ["What is the full form of RAM?", "random access memory"],
-        2: ["What is the full form of ROM?", "read only memory"],
-        3: ["What is the full form of AI?", "artificial intelligence"],
-        4: ["What does CPU stand for?", "central processing unit"],
-        5: ["What does IP stand for in IP Address?", "internet protocol"],
-        6: ["What is the full form of USB", "universal serial bus"],
-        7: ["Which language does a computer understand", "binary"],
-        8: ["What does the Internet prefix WWW stand for?", "world wide web"],
-        9: ["What does WLAN stand for?", "wireless local area network"],
-        10: ["What is the full form of URL?", "universal resource locator"],
+        1: ['What is the full form of RAM?', 'random access memory'],
+        2: ['What is the full form of ROM?', 'read only memory'],
+        3: ['What is the full form of AI?', 'artificial intelligence'],
+        4: ['What does CPU stand for?', 'central processing unit'],
+        5: ['What does IP stand for in IP Address?', 'internet protocol'],
+        6: ['What is the full form of USB', 'universal serial bus'],
+        7: ['Which language does a computer understand', 'binary'],
+        8: ['What does the Internet prefix WWW stand for?', 'world wide web'],
+        9: ['What does WLAN stand for?', 'wireless local area network'],
+        10: ['What is the full form of URL?', 'universal resource locator'],
     }
 
     function randomNumber(min, max) {
-        return Math.round(Math.random() * (max - min) + min);
+        return Math.round(Math.random() * (max - min) + min)
     }
-    const question = randomNumber(1,10)
-    document.querySelector('.question').innerHTML = "Q) " + questions[question][0]
+    const question = randomNumber(1, 10)
+    document.querySelector('.question').innerHTML =
+        'Q) ' + questions[question][0]
 
     const answerForm = document.querySelector('.answer-form')
-    const answerField = document.querySelector('.answer-field') 
+    const answerField = document.querySelector('.answer-field')
     const infoForm = document.querySelector('.correct')
-    answerForm.addEventListener('submit', (e) => {
+    answerForm.addEventListener('submit', e => {
         e.preventDefault()
-        if (answerField.value.toLowerCase() === questions[question][1].toLowerCase()) {
-            document.querySelector('.incorrect').style.display = "none"
-            document.querySelector('.correct').style.display = "block"
-            document.querySelector('.qa').style.display = "none"
+        if (
+            answerField.value.toLowerCase() ===
+            questions[question][1].toLowerCase()
+        ) {
+            document.querySelector('.incorrect').style.display = 'none'
+            document.querySelector('.correct').style.display = 'block'
+            document.querySelector('.qa').style.display = 'none'
         } else {
-            document.querySelector('.incorrect').style.display = "block"
-            document.querySelector('.correct').style.display = "none"
+            document.querySelector('.incorrect').style.display = 'block'
+            document.querySelector('.correct').style.display = 'none'
         }
     })
 
-    infoForm.addEventListener('submit', (e) => {
+    infoForm.addEventListener('submit', e => {
         e.preventDefault()
         let name = document.getElementById('name').value
         let grade = document.getElementById('class').value
@@ -54,24 +58,52 @@ window.onload = () => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "name": name,
-                "class": grade,
-                "section": section,
-                "dps_admission_number": adm,
-                "discord": discord,
-                "phone": phone
+                name: name,
+                class: grade,
+                section: section,
+                dps_admission_number: adm,
+                discord: discord,
+                phone: phone,
+            }),
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    document.querySelector('.Discord').style.display = 'flex'
+                    document.querySelector('.correct').style.display = 'none'
+                    localStorage.setItem('filled', true)
+                } else {
+                    fetch('https://it-council-api2.herokuapp.com/backend_', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            name: name,
+                            class: grade,
+                            section: section,
+                            dps_admission_number: adm,
+                            discord: discord,
+                            phone: phone,
+                        }),
+                    })
+                        .then(response => {
+                            if (response.status === 200) {
+                                document.querySelector(
+                                    '.Discord',
+                                ).style.display = 'flex'
+                                document.querySelector(
+                                    '.correct',
+                                ).style.display = 'none'
+                                localStorage.setItem('filled', true)
+                            } else {
+                                alert(
+                                    'There was an issue while adding your registration. Please fill the form again in a while and submit.',
+                                )
+                            }
+                        })
+                        .catch(error => console.error(error))
+                }
             })
-        })
-        .then(response => {
-            if(response.status === 200){
-                document.querySelector('.Discord').style.display = "flex"
-                document.querySelector('.correct').style.display = "none"
-                localStorage.setItem('filled', true)
-            }
-            else{
-                alert('There was an issue in registering you. Please fill the form again.')
-            }
-        })
-        .catch(error => console.log(error))
+            .catch(error => console.error(error))
     })
 }
